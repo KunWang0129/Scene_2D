bc996cd8ea7a9b261f135cd3e2fb8291461d2bb4
KunWang0129 <103203141+KunWang0129@users.noreply.github.com>
2024-05-03 15:39:48 -0700

    rag implemented

---
+++ scripts/requirements.sh
@@ -0,0 +1,6 @@
+conda create -n "Scene2D" python=3.11 --yes
+
+# Source the conda environment
+source /root/anaconda3/etc/profile.d/conda.sh
+conda activate Scene2D
+pip install -r requirements.txt
---
+++ scripts/run.sh
@@ -0,0 +1,4 @@
+cd ..
+python3 main.py
+python3 code.py
+open scene.png
