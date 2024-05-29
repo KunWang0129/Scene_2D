616362aa6136bf77d88419ebbe203c659a3e3e53
ChenYenRu <rebecca946487@gmail.com>
2024-05-29 16:36:51 -0700

    added VLM Evaluation
    Co-authored-by: Mayank Jain <jmayank23@users.noreply.github.com>
    Co-authored-by: rgiaca <rgiaca@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,50 @@
 from utils.generator import SceneGenerator
+from tools.vlm import VLM
+import os
+
+DIR = 'threecars'
+# IMAGE_SUBDIR = 'rendered_scene'
+IMAGE_SUBDIR = 'rendered_scene'
+descriptions = dict()
+filtered_images = []
 
 print("Coming up with candidate scenes ....")
-SceneGenerator().run('')
+# SceneGenerator().run('')
+
+# Get the description first
+example_files = [f for f in os.listdir(DIR) if f.endswith('_0.py') and f != '__init__.py']
+# example_files = [f for f in os.listdir(DIR) if f.endswith('.py') and f != '__init__.py']
+for file_name in example_files:
+    # Construct the full file path
+    file_path = os.path.join(DIR, file_name)
+
+    # Open the file and execute the code
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    desc = "".join(lines)
+    first = desc.find("##@##")
+    second = desc.find("##@##", first + 6)
+    description = desc[first + 6 : second]
+    description = description.replace("description = ", "").replace("'", "")
+    image_files = [f"{DIR}/{IMAGE_SUBDIR}/{f}" for f in os.listdir(f"{DIR}") if f.startswith(f"{f.split('_')[0]}_") and f != file_name]
+    descriptions[file_name[:-5]] = {"description": description, "path": image_files}
+
+vlm = VLM()
+for key, value in descriptions.items():
+    print(f"Creating scene for {key} ....")
+    print(value["path"])
+    response = vlm.run(value["path"], value["description"])
+    print(response)
+    filtered_images.append(response)
+    print("\n")
+
+# TODO: store the filtered images in a directory
 
 # Create a scene with five trees on top of a grass floor
 # Create a scene with a sailing boat on the sea in a sunny day
+
+# vlm = VLM()
+# desc = "Create a scene with three cars parked on the street"
+# image_paths = ["threecars/threecars1.png", "threecars/threecars2.png", "threecars/threecars3.png", "threecars/threecars4.png", "threecars/threecars5.png"]
+# response = vlm.run(image_paths, desc)
+# print(response.content[0].text)
--- render_examples.py
+++ render_examples.py
@@ -3,13 +3,14 @@ from utils.Scene import Scene
 from utils.Shape import Circle, Rectangle, Triangle
 
 # Get a list of all files in the 'examples' directory
-example_files = [f for f in os.listdir('examples') if f.endswith('.py') and f != '__init__.py']
-example_files = ['chair.py']
+# example_files = [f for f in os.listdir('examples') if f.endswith('.py') and f != '__init__.py']
+example_files = [f for f in os.listdir('test_examples') if f.endswith('.py') and f != '__init__.py']
+# example_files = ['code8_0.py']
 
 # Loop through each file
 for file_name in example_files:
     # Construct the full file path
-    file_path = os.path.join('examples', file_name)
+    file_path = os.path.join('test_examples', file_name)
 
     # Open the file and execute the code
     with open(file_path, 'r') as file:
@@ -17,4 +18,5 @@ for file_name in example_files:
 
     # Assuming that the 'scene' variable is defined in each file,
     # render the scene and save it as an image
-    image = scene.render(f'examples/rendered_scene/{file_name[:-3]}.png')
+    print(file_name[:-3])
+    image = scene.render(f'test_examples/rendered_scene/{file_name[:-3]}.png')
