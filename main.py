ec2a3350b7ea571d053e2ace708af3a369c93e87
ChenYenRu <rebecca946487@gmail.com>
2024-05-31 15:38:26 -0700

    Enabled main.py to save selected images to the "selected_images" folder

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from utils.generator import SceneGenerator
 from tools.vlm import VLM
 import os
+import shutil
 
 DIR = 'threecars'
-# IMAGE_SUBDIR = 'rendered_scene'
 IMAGE_SUBDIR = 'rendered_scene'
 descriptions = dict()
 filtered_images = []
@@ -12,7 +12,9 @@ print("Coming up with candidate scenes ....")
 # SceneGenerator().run('')
 
 # Get the description first
-example_files = [f for f in os.listdir(DIR) if f.endswith('_0.py') and f != '__init__.py']
+example_files = [f for f in os.listdir(
+    DIR) if f.endswith('_0.py') and f != '__init__.py']
+print(example_files)
 # example_files = [f for f in os.listdir(DIR) if f.endswith('.py') and f != '__init__.py']
 for file_name in example_files:
     # Construct the full file path
@@ -24,10 +26,12 @@ for file_name in example_files:
     desc = "".join(lines)
     first = desc.find("##@##")
     second = desc.find("##@##", first + 6)
-    description = desc[first + 6 : second]
+    description = desc[first + 6: second]
     description = description.replace("description = ", "").replace("'", "")
-    image_files = [f"{DIR}/{IMAGE_SUBDIR}/{f}" for f in os.listdir(f"{DIR}") if f.startswith(f"{f.split('_')[0]}_") and f != file_name]
-    descriptions[file_name[:-5]] = {"description": description, "path": image_files}
+    image_files = [f"{DIR}/{IMAGE_SUBDIR}/{f}" for f in os.listdir(f"{DIR}/{IMAGE_SUBDIR}") if f.startswith(
+        f"{f.split('_')[0]}") and f.endswith("png") and f != file_name]
+    descriptions[file_name[:-5]] = {"description": description,
+                                    "path": image_files, "file_name": file_name[:-5]}
 
 vlm = VLM()
 for key, value in descriptions.items():
@@ -36,15 +40,12 @@ for key, value in descriptions.items():
     response = vlm.run(value["path"], value["description"])
     print(response)
     filtered_images.append(response)
+    with open(f"selected_images/{value['file_name']}.png", 'wb') as f:
+        shutil.copy(response, f"selected_images/{value['file_name']}.png")
     print("\n")
 
-# TODO: store the filtered images in a directory
-
-# Create a scene with five trees on top of a grass floor
-# Create a scene with a sailing boat on the sea in a sunny day
-
-# vlm = VLM()
-# desc = "Create a scene with three cars parked on the street"
-# image_paths = ["threecars/threecars1.png", "threecars/threecars2.png", "threecars/threecars3.png", "threecars/threecars4.png", "threecars/threecars5.png"]
-# response = vlm.run(image_paths, desc)
-# print(response.content[0].text)
+# Copy the filtered images to directory "selected_images"
+os.makedirs('selected_images', exist_ok=True)
+for i, image_path in enumerate(filtered_images):
+    with open(f"selected_images/{i}.png", 'wb') as f:
+        shutil.copy(image_path, f"selected_images/{i}.png")
